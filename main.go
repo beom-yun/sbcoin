e901ee9ef1c9b430858bee0ad2d45a3f3154e80f
beom-yun <scds0326@naver.com>
2022-06-30 01:31:34 +0900

    FlagSet

--- main.go
+++ main.go
@@ -17,13 +17,4 @@ func main() {
 	if len(os.Args) < 2 {
 		usage()
 	}
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		fmt.Println("Start REST API")
-	default:
-		usage()
-	}
 }
