70c44dbef2aac561896dfb1d626688ce24377720
beom-yun <scds0326@naver.com>
2022-06-30 00:30:41 +0900

    Parsing Commands

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
-	"github.com/beom-yun/sbcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcone to SB Coin!\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	// go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
