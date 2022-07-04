394e69ba2107a9a9fbed50ffc0565e3b7e9d473f
beom-yun <scds0326@naver.com>
2022-07-05 01:49:20 +0900

    Persisting The Blockchain

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/beom-yun/sbcoin/blockchain"
 
 func main() {
-	blockchain.Blockchain()
+	blockchain.Blockchain().AddBlock("First")
+	blockchain.Blockchain().AddBlock("Second")
+	blockchain.Blockchain().AddBlock("Third")
 }
