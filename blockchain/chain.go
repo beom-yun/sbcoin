24091fe610484c50b2e22d0bced84efd786efa43
beom-yun <scds0326@naver.com>
2022-07-06 13:00:52 +0900

    Restoring the Blockchain

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
 	"sync"
 
 	"github.com/beom-yun/sbcoin/db"
@@ -14,6 +17,11 @@ type blockchain struct {
 
 var b *blockchain
 var once sync.Once
+
+func (b *blockchain) restore(data []byte) {
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	utils.HandleErr(decoder.Decode(b))
+}
 
 func (b *blockchain) persist() {
 	db.SaveBlockChain(utils.ToBytes(b))
@@ -30,8 +38,16 @@ func Blockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
 			b = &blockchain{"", 0}
-			b.AddBlock("Genesis")
+			fmt.Printf("NewestHash: %s\nHeight: %d\n", b.NewestHash, b.Height)
+			checkpoint := db.Checkpoint()
+			if checkpoint == nil {
+				b.AddBlock("Genesis")
+			} else {
+				fmt.Println("Restoring...")
+				b.restore(checkpoint)
+			}
 		})
 	}
+	fmt.Printf("NewestHash: %s\nHeight: %d", b.NewestHash, b.Height)
 	return b
 }
